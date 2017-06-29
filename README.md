9d859d39da4745439812273281630517dee7a2e1
Karol Popławski <karolpoplawski@gmail.com>
2017-06-29 16:46:29 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## Vue Events Board
+
+
+
+## Installation
+
+1. Run `npm install`.
+2. Run `node server.js`.
+3. Visit [http://localhost:8080](http://localhost:8080).
+
---
+++ backend/Events.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    id: 1,
+    title: 'Date',
+    detail: 'Date with girlfriend',
+    date: '2016-01-14'
+  },
+  {
+    id: 2,
+    title: 'Web dev challenge',
+    detail: 'Hackaton',
+    date: '2017-06-04'
+  }
+];
